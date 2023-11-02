747258034bc5e4e914f5771c24e440c3816b3faf
Jonas Geiler <git@jonasgeiler.com>
2023-11-02 22:17:52 +0100

    refactor: simplified parser

--- src/Parser.php
+++ src/Parser.php
@@ -28,267 +28,81 @@ class Parser {
 
 		$programLength = strlen($program);
 		$instruction = $rootInstruction;
-		$loopStack = [];
+		$loopStartStack = [];
 		for ($pi = 0; $pi < $programLength; $pi++) {
-			switch ($program[$pi]) {
+			$op = $program[$pi];
+
+			switch ($op) {
 				case $opJumpRight:
-					// This makes sure to go back to the previous instruction if the
-					// current one is a 'add 0' instruction, which doesn't do anything.
-					if (
-						// Check if the current instruction is 'add 0':
-						$instruction->opcode === Opcode::Add
-						&& $instruction->amount === 0
-						// Check if the previous instruction is a jump instruction:
-						&& $instruction->previous->opcode === Opcode::Jump
-					) {
-						// Reset back to the previous instruction and continue there.
-						$instruction->previous->next = null;
-						$instruction = $instruction->previous;
-					}
-
-					if (
-						// First we check if the current instruction is already
-						// a Jump instruction:
-						$instruction->opcode === Opcode::Jump
-						// And then we confirm that increasing the instruction
-						// amount doesn't exceed the max integer value:
-						&& $instruction->amount + 1 < PHP_INT_MAX
-					) {
-						// It's safe to just increase the amount and continue.
-						$instruction->amount += 1;
-					} else {
-						// Either the current instruction isn't a Jump instruction,
-						// or the amount exceeds the max integer value.
-						// In this case we overwrite/create a new instruction:
-
-						if (
-							// Only create the next instruction if the current
-							// one isn't still empty:
-							$instruction->opcode !== null
-							&& (
-								// And also prevent creating the next instruction
-								// if the current one is 'add 0', which doesn't
-								// do anything therefore we can overwrite it.
-								$instruction->opcode !== Opcode::Add
-								|| $instruction->amount !== 0
-							)
-						) {
-							// Create the next instruction
-							$instruction->next = new Instruction();
-							$instruction->next->previous = $instruction;
-							$instruction = $instruction->next;
-						}
-
-						// Initialize the instruction with the opcode and amount
-						$instruction->opcode = Opcode::Jump;
-						$instruction->amount = 1;
-					}
-					break;
-
 				case $opJumpLeft:
-					if (
-						$instruction->opcode === Opcode::Add
-						&& $instruction->amount === 0
-						&& $instruction->previous->opcode === Opcode::Jump
-					) {
-						$instruction->previous->next = null;
-						$instruction = $instruction->previous;
-					}
+					self::revertInstructionIfNoop($instruction);
 
+					$amountDiff = $op === $opJumpRight ? 1 : -1;
 					if (
 						$instruction->opcode === Opcode::Jump
-						&& $instruction->amount - 1 > PHP_INT_MIN
+						&& $instruction->amount + $amountDiff <= PHP_INT_MAX
+						&& $instruction->amount + $amountDiff > PHP_INT_MIN
 					) {
-						$instruction->amount -= 1;
+						$instruction->amount += $amountDiff;
 					} else {
-						if (
-							$instruction->opcode !== null
-							&& (
-								$instruction->opcode !== Opcode::Add
-								|| $instruction->amount !== 0
-							)
-						) {
-							$instruction->next = new Instruction();
-							$instruction->next->previous = $instruction;
-							$instruction = $instruction->next;
-						}
-
-						$instruction->opcode = Opcode::Jump;
-						$instruction->amount = -1;
+						self::initInstruction(
+							$instruction,
+							Opcode::Jump,
+							$amountDiff,
+						);
 					}
 					break;
 
 				case $opAddIncrease:
-					if (
-						$instruction->opcode === Opcode::Jump
-						&& $instruction->amount === 0
-						&& $instruction->previous->opcode === Opcode::Add
-					) {
-						$instruction->previous->next = null;
-						$instruction = $instruction->previous;
-					}
-
-					if (
-						$instruction->opcode === Opcode::Add
-						&& $instruction->amount + 1 < PHP_INT_MAX
-					) {
-						$instruction->amount += 1;
-					} else {
-						if (
-							$instruction->opcode !== null
-							&& (
-								$instruction->opcode !== Opcode::Jump
-								|| $instruction->amount !== 0
-							)
-						) {
-							$instruction->next = new Instruction();
-							$instruction->next->previous = $instruction;
-							$instruction = $instruction->next;
-						}
-
-						$instruction->opcode = Opcode::Add;
-						$instruction->amount = 1;
-					}
-					break;
-
 				case $opAddDecrease:
-					if (
-						$instruction->opcode === Opcode::Jump
-						&& $instruction->amount === 0
-						&& $instruction->previous->opcode === Opcode::Add
-					) {
-						$instruction->previous->next = null;
-						$instruction = $instruction->previous;
-					}
+					self::revertInstructionIfNoop($instruction);
 
+					$amountDiff = $op === $opAddIncrease ? 1 : -1;
 					if (
 						$instruction->opcode === Opcode::Add
-						&& $instruction->amount - 1 > PHP_INT_MIN
+						&& $instruction->amount + $amountDiff <= PHP_INT_MAX
+						&& $instruction->amount + $amountDiff > PHP_INT_MIN
 					) {
-						$instruction->amount -= 1;
+						$instruction->amount += $amountDiff;
 					} else {
-						if (
-							$instruction->opcode !== null
-							&& (
-								$instruction->opcode !== Opcode::Jump
-								|| $instruction->amount !== 0
-							)
-						) {
-							$instruction->next = new Instruction();
-							$instruction->next->previous = $instruction;
-							$instruction = $instruction->next;
-						}
-
-						$instruction->opcode = Opcode::Add;
-						$instruction->amount = -1;
+						self::initInstruction(
+							$instruction,
+							Opcode::Add,
+							$amountDiff,
+						);
 					}
 					break;
 
 				case $opLoopStart:
-					if (
-						$instruction->opcode !== null
-						&& (
-							(
-								$instruction->opcode !== Opcode::Jump
-								&& $instruction->opcode !== Opcode::Add
-							)
-							|| $instruction->amount !== 0
-						)
-					) {
-						$instruction->next = new Instruction();
-						$instruction->next->previous = $instruction;
-						$instruction = $instruction->next;
-					}
-
-					$instruction->opcode = Opcode::LoopStart;
-					$instruction->amount = null;
-
-					// Just add the current instruction to the loop stack.
-					$loopStack[] = $instruction;
+					self::initInstruction($instruction, Opcode::LoopStart);
+					$loopStartStack[] = $instruction;
 					break;
 
 				case $opLoopEnd:
-					// Check if the loop stack is currently empty, which means
-					// there are too many ']' in the program:
-					if (count($loopStack) === 0) {
-						// Throw a syntax error.
-						throw new \Exception('Unmatched ]'); // TODO: Use custom error
+					if (empty($loopStartStack)) {
+						// TODO: Use custom error
+						throw new \Exception('Unmatched ]');
 					}
 
 					// TODO: Skip if empty loop
-
-					if (
-						$instruction->opcode !== null
-						&& (
-							(
-								$instruction->opcode !== Opcode::Jump
-								&& $instruction->opcode !== Opcode::Add
-							)
-							|| $instruction->amount !== 0
-						)
-					) {
-						$instruction->next = new Instruction();
-						$instruction->next->previous = $instruction;
-						$instruction = $instruction->next;
-					}
-
-					$instruction->opcode = Opcode::LoopEnd;
-					$instruction->amount = null;
-
-					// Get the last item of the loop stack, which should be the
-					// matching LoopStart instruction, and then create a link.
-					$instruction->match = array_pop($loopStack);
+					self::initInstruction($instruction, Opcode::LoopEnd);
+					$instruction->match = array_pop($loopStartStack);
 					$instruction->match->match = $instruction;
 					break;
 
 				case $opOutput:
-					if (
-						$instruction->opcode !== null
-						&& (
-							(
-								$instruction->opcode !== Opcode::Jump
-								&& $instruction->opcode !== Opcode::Add
-							)
-							|| $instruction->amount !== 0
-						)
-					) {
-						$instruction->next = new Instruction();
-						$instruction->next->previous = $instruction;
-						$instruction = $instruction->next;
-					}
-
-					$instruction->opcode = Opcode::Output;
-					$instruction->amount = null;
+					self::initInstruction($instruction, Opcode::Output);
 					break;
 
 				case $opInput:
-					if (
-						$instruction->opcode !== null
-						&& (
-							(
-								$instruction->opcode !== Opcode::Jump
-								&& $instruction->opcode !== Opcode::Add
-							)
-							|| $instruction->amount !== 0
-						)
-					) {
-						$instruction->next = new Instruction();
-						$instruction->next->previous = $instruction;
-						$instruction = $instruction->next;
-					}
-
-					$instruction->opcode = Opcode::Input;
-					$instruction->amount = null;
+					self::initInstruction($instruction, Opcode::Input);
 					break;
-
-				default:
-					continue 2;
 			}
 		}
 
 		// Check if the loop stack is not empty after parsing, which means
 		// there are too many '[' in the program:
-		if (count($loopStack) !== 0) {
+		if (!empty($loopStartStack)) {
 			// Throw a syntax error.
 			throw new \Exception('Unmatched [');
 		}
@@ -319,5 +133,56 @@ class Parser {
 		}
 
 		return $rootInstruction;
+	}
+
+	protected static function revertInstructionIfNoop(
+		Instruction &$instruction,
+	) {
+		if (
+			$instruction->amount === 0
+			&& (
+				(
+					$instruction->opcode === Opcode::Jump
+					&& $instruction->previous->opcode === Opcode::Add
+				)
+				|| (
+					$instruction->opcode === Opcode::Add
+					&& $instruction->previous->opcode === Opcode::Jump
+				)
+			)
+		) {
+			$instruction->previous->next = null;
+			$instruction = $instruction->previous;
+		}
+	}
+
+	protected static function initInstruction(
+		Instruction &$instruction,
+		Opcode $opcode,
+		?int $amount = null,
+	): void {
+		if (
+			// Only create the next instruction if the current
+			// one isn't still empty:
+			$instruction->opcode !== null
+			&& (
+				// And also prevent creating the next instruction
+				// if the current one is 'add 0', which doesn't
+				// do anything therefore we can overwrite it.
+				(
+					$instruction->opcode !== Opcode::Jump
+					&& $instruction->opcode !== Opcode::Add
+				)
+				|| $instruction->amount !== 0
+			)
+		) {
+			// Create the next instruction
+			$instruction->next = new Instruction();
+			$instruction->next->previous = $instruction;
+			$instruction = $instruction->next;
+		}
+
+		$instruction->opcode = $opcode;
+		$instruction->amount = $amount;
 	}
 }
